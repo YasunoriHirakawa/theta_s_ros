b2a7138192ace675348e7a0fbb9d630219b54611
Hori3538 <yu.m.h.94612@gmail.com>
2024-02-27 11:06:12 +0900

    create brushee branch

--- include/theta_s_ros/image_converter.hpp
+++ include/theta_s_ros/image_converter.hpp
@@ -1,30 +1,21 @@
 #ifndef __IMAGE_CONVERTER_HPP
 #define __IMAGE_CONVERTER_HPP
 
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/opencv.hpp>
 #include <ros/ros.h>
-#include <sensor_msgs/CompressedImage.h>
 #include <sensor_msgs/Image.h>
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
 
-namespace theta_s_ros {
 class ImageConverter {
 public:
     ImageConverter();
-    void convert_to_equirectangular(cv::Mat& cv_image, cv::Mat& equirectangular_image);
-    void publish_image(const cv::Mat& src_image, ros::Publisher pub, const std_msgs::Header& header);
     void image_callback(const sensor_msgs::ImageConstPtr&);
     void process();
-
 private:
-    ros::NodeHandle nh_;
-    ros::NodeHandle private_nh_;
-    ros::Subscriber image_sub_;
-    ros::Publisher equirectangular_image_pub_;
-    std::vector<ros::Publisher> cubemap_image_pubs_;
-    std::vector<int> crop_y_;
-    bool unmerge_top_and_bottom_;
+    ros::NodeHandle nh;
+    ros::NodeHandle private_nh;
+    ros::Subscriber image_sub;
+    ros::Publisher image_pub;
 };
-} // namespace theta_s_ros
 
 #endif // __IMAGE_CONVERTER_HPP
--- src/image_converter.cpp
+++ src/image_converter.cpp
@@ -1,53 +1,13 @@
 #include "theta_s_ros/image_converter.hpp"
 
 #include "opencv_theta_s/EquirectangularConversion/ThetaConversion.hpp"
-#include "panorama2cubemap/src/Panorama2Cubemap.hpp"
 
-namespace theta_s_ros {
-ImageConverter::ImageConverter()
-    : private_nh_("~")
-{
-    using namespace std::literals::string_literals;
-
-    private_nh_.param("crop_y", crop_y_, std::vector<int>());
-    private_nh_.param("unmerge_top_and_bottom", unmerge_top_and_bottom_, false);
-    if (crop_y_.size() != 0 && crop_y_.size() != 2) {
-        ROS_ERROR("crop_y_ must be a list of size 2");
-        ros::shutdown();
-    }
-
-    image_sub_ = nh_.subscribe(
-        "/camera/image_raw", 1, &ImageConverter::image_callback, this, ros::TransportHints().reliable().tcpNoDelay());
-    equirectangular_image_pub_ = nh_.advertise<sensor_msgs::CompressedImage>("/equirectangular/image_raw/compressed", 1);
-    for (const auto& face : { "right"s, "left"s, "top"s, "bottom"s, "front"s, "back"s, "merged"s }) {
-        cubemap_image_pubs_.push_back(nh_.advertise<sensor_msgs::CompressedImage>(
-            "/cubemap/" + face + "/image_raw/compressed", 1));
-    }
+ImageConverter::ImageConverter() : private_nh("~") {
+    image_sub = nh.subscribe("/camera/image_raw", 1, &ImageConverter::image_callback, this);
+    image_pub = nh.advertise<sensor_msgs::Image>("/equirectangular/image_raw", 1);
 }
 
-void ImageConverter::convert_to_equirectangular(cv::Mat& cv_image, cv::Mat& equirectangular_image)
-{
-    static ThetaConversion theta_conversion(cv_image.cols, cv_image.rows);
-    theta_conversion.doConversion(cv_image);
-    static auto rect = crop_y_.size() == 0 ? cv::Rect(0, 0, cv_image.cols, cv_image.rows)
-                                           : cv::Rect(0, crop_y_[0], cv_image.cols, crop_y_[1] - crop_y_[0] + 1);
-    cv::Mat cv_image_cropped(cv_image, rect);
-    equirectangular_image = std::move(cv_image_cropped);
-}
-
-void ImageConverter::publish_image(const cv::Mat& src_image, ros::Publisher pub, const std_msgs::Header& header)
-{
-    sensor_msgs::CompressedImage image_msg;
-    image_msg.header = header;
-    std::vector<unsigned char> buffer;
-    image_msg.format = "jpeg";
-    cv::imencode(".jpg", src_image, buffer);
-    image_msg.data.swap(buffer);
-    pub.publish(image_msg);
-}
-
-void ImageConverter::image_callback(const sensor_msgs::ImageConstPtr& received_image)
-{
+void ImageConverter::image_callback(const sensor_msgs::ImageConstPtr& received_image) {
     ROS_DEBUG("start image_callback");
     double start_time = ros::Time::now().toSec();
 
@@ -59,40 +19,41 @@ void ImageConverter::image_callback(const sensor_msgs::ImageConstPtr& received_i
         ROS_ERROR("cv_bridge exception: %s", ex.what());
         return;
     }
-    cv::Mat cv_image(cv_image_ptr->image.rows, cv_image_ptr->image.cols, cv_image_ptr->image.type());
-    cv_image = cv_image_ptr->image;
+    cv::Mat cv_image = cv_image_ptr->image;
+    // thetaから得られる生画像に含まれるいらない領域を省く
+    // 720*1280 -> 640*1280
+    cv_image = cv_image(cv::Rect(0, 0, cv_image.cols, cv_image.cols/2));
 
-    ROS_DEBUG("image conversion");
-    cv::Mat equirectangular_image;
-    std::vector<cv::Mat> cubemap_images;
-    cv::Mat merged_image;
-    convert_to_equirectangular(cv_image, equirectangular_image);
-    pano2cube(cv_image, cubemap_images, merged_image, unmerge_top_and_bottom_);
-    cubemap_images.push_back(merged_image);
+    ROS_DEBUG("fisheye image to equirectangular image");
+    static ThetaConversion theta_conversion(cv_image.cols, cv_image.rows);
+    theta_conversion.doConversion(cv_image);
 
-    ROS_DEBUG("publish images");
-    publish_image(equirectangular_image, equirectangular_image_pub_, received_image->header);
-    for (std::size_t i = 0; i < cubemap_image_pubs_.size(); ++i) {
-        publish_image(cubemap_images[i], cubemap_image_pubs_[i], received_image->header);
+    ROS_DEBUG("cv image to output ros image");
+    sensor_msgs::ImagePtr output_image = cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_image).toImageMsg();
+    output_image->header = received_image->header;
+
+    // thetaから得られる生画像にはstampが含まれないので
+    // headerのstampがないときの処理
+    if(output_image->header.stamp.sec == 0){
+        output_image->header.stamp = ros::Time::now();
     }
+
+    image_pub.publish(output_image);
 
     ROS_INFO("[image_converter:image_callback] elapsed time : %f [sec]", ros::Time::now().toSec() - start_time);
 
     return;
 }
 
-void ImageConverter::process()
-{
+void ImageConverter::process() {
     ROS_DEBUG("start process");
     ros::spin();
     return;
 }
-} // namespace image_converter
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     ros::init(argc, argv, "convert_ros_image_to_cv_image");
-    theta_s_ros::ImageConverter image_convertor;
+    ImageConverter image_convertor;
     image_convertor.process();
     return 0;
 }
